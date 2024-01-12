b61c6d6b0e03ed23d435b5bf3e71444aabab5285
Dheeraj Kumar Kondapalli <dheerajkumar200005@gmail.com>
2024-01-11 20:54:12 -0600

    first push

--- cart-api/src/main/java/com/ecom/cartapi/DAO/ProductRepo.java
+++
@@ -1,7 +0,0 @@
-package com.ecom.cartapi.DAO;
-
-import com.ecom.cartapi.Model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepo extends JpaRepository<Product, String> {
-}
